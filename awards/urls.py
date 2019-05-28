883f2b69862bf14f6194115792110ab4b61dc6e8
lilianwaweru <lilowesh.lw@gmail.com>
2019-05-28 17:48:21 +0300

    created url for profile

--- awards/urls.py
+++ awards/urls.py
@@ -10,6 +10,8 @@ urlpatterns=[
     url(r'^project/$',views.ProjectsUpload,name = 'ProjectForm'),
     url(r'^review/$',views.review, name = 'VoteForm'),
     url(r'^profile/$',views.edit_profile, name = 'EditProfile'),
+    url(r'^api/profile/$',views.profile_list.as_view())
+    url(r'^api/project/$',views.project_list.as_view())
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

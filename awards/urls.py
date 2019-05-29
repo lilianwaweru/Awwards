899fe5d1990235173cdf8e3b192502c91897ac19
lilianwaweru <lilowesh.lw@gmail.com>
2019-05-29 11:16:00 +0300

    added styling card for projects

--- awards/urls.py
+++ awards/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns=[
@@ -11,7 +12,8 @@ urlpatterns=[
     url(r'^review/$',views.review, name = 'VoteForm'),
     url(r'^profile/$',views.edit_profile, name = 'EditProfile'),
     url(r'^api/profile/$', views.profile_list.as_view()),
-    url(r'^api/projects/$', views.project_list.as_view())
+    url(r'^api/projects/$', views.project_list.as_view()),
+    url(r'^api-token-auth/',obtain_auth_token)
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- awward/settings.py
+++ awward/settings.py
@@ -134,3 +134,10 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
